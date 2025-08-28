1ba257b30d62d7540980d3d45e772713d8249fb0
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-28 16:11:48 -0700

    Bundle via structural typing

--- hdl/src/main.scala
+++ hdl/src/main.scala
@@ -22,9 +22,32 @@ sealed class UInt(val w: Width) extends Signal:
 sealed class UIntLit(val w: Width)(val v: Int) extends Signal:
   override def toString(): String = s"UIntLit($v($w.W))"
 
+class Bundle(elems: (String, Any)*) extends Selectable:
+  private val fields = elems.toMap
+  def selectDynamic(name: String): Any = fields(name)
+
 object Main:
   def main(args: Array[String]): Unit =
     println("Hello World")
 
     println(s"${UInt(Width(3))}")
     println(s"${UIntLit(Width(3))(4)}")
+
+    class MyBundle(x: Int, y: Int) extends Bundle:
+      val a = UInt(Width(x))
+      val b = UInt(Width(y))
+
+    val my_bundle = new MyBundle(2, 3)
+
+    println(s"${my_bundle}")
+    println(s"${my_bundle.a}")
+    println(s"${my_bundle.b}")
+
+    class NestedBundle(x: Int, y: Int, z: Int) extends Bundle:
+      val inner = new MyBundle(x, y)
+      val outer = UInt(Width(z))
+
+    val nested_bundle = new NestedBundle(2, 3, 4)
+    println(s"${nested_bundle.outer}")
+    println(s"${nested_bundle.inner.a}")
+    println(s"${nested_bundle.inner.b}")
