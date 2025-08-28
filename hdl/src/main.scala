219511d88c18e64dbf293cfb615be04bb2b9e489
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-27 17:58:43 -0700

    Attempt typeclass der

--- hdl/src/main.scala
+++ hdl/src/main.scala
@@ -4,6 +4,7 @@ import scala.compiletime.{erasedValue, constValue, summonInline}
 import scala.deriving.*
 import scala.compiletime.ops.int.*
 import scala.annotation.targetName
+import scala.compiletime.erasedValue
 
 
 sealed class Width(val value: Int):
@@ -12,15 +13,16 @@ sealed class Width(val value: Int):
 object Width:
   def apply(x: Int): Width = new Width(x)
 
-sealed trait Signal
+sealed trait Flavor
+sealed trait Bit extends Flavor
+sealed trait Lit  extends Flavor
+sealed trait Dpi  extends Flavor
 
-sealed case class UInt(val w: Width) extends Signal:
-  override def toString(): String = s"UInt($w)"
+sealed trait Signal[F <: Flavor]
 
-sealed case class UIntLit(val w: Width, val v: Int) extends Signal:
-  override def toString(): String = s"UIntLit($w, $v)"
+sealed case class UInt[F <: Flavor](val w: Width) extends Signal[F]
 
-trait Bundle extends Signal
+trait Bundle[F <: Flavor] extends Signal[F]
 
 object Main:
   def main(args: Array[String]): Unit =
@@ -28,10 +30,12 @@ object Main:
     println(s"UInt(3): $uint_3")
 
 
-    class MyBundle(w1: Int, w2: Int) extends Bundle:
-      val a = UInt(Width(w1))
-      val b = UInt(Width(w2))
+    class MyBundle[F <: Flavor](w1: Int, w2: Int) extends Bundle:
+      val a = UInt[F](Width(w1))
+      val b = UInt[F](Width(w2))
 
-    val my_bundle = new MyBundle(2, 3)
+    val my_bundle = new MyBundle[Bit](2, 3)
     println(s"my_bundle ${my_bundle} ${my_bundle.a} ${my_bundle.b}")
 
+    val my_bundle_dpi = new MyBundle[Dpi](2, 3)
+    println(s"my_bundle_dpi ${my_bundle_dpi} ${my_bundle_dpi.a} ${my_bundle_dpi.b}")
