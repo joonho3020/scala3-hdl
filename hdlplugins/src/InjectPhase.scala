35661ede6268628b3c11f6caeca0880ded045224
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-30 13:49:10 -0700

    WIP

--- hdlplugins/src/InjectPhase.scala
+++ hdlplugins/src/InjectPhase.scala
@@ -10,6 +10,7 @@ import dotty.tools.dotc.core.Names._
 import dotty.tools.dotc.core.StdNames._
 import dotty.tools.dotc.core.Flags._
 import dotty.tools.dotc.core.Types._
+import dotty.tools.dotc.ast.untpd.ModuleDef
 
 final class InjectPhase extends PluginPhase {
   override val phaseName  = "bundle-lit-inject"
@@ -17,7 +18,30 @@ final class InjectPhase extends PluginPhase {
   override val runsBefore = Set("erasure")
 
   override def transformPackageDef(tree: PackageDef)(using Context): Tree = {
-    println(s"[bundle-lit] visiting ${tree.pid.show}")
-    tree
+// println(s"[bundle-lit] visiting ${tree.pid.show}")
+
+    // Collect bundle classes in this package
+    val bundleOwners = scala.collection.mutable.HashSet[Symbol]()
+    val bundleSymbol: ClassSymbol = requiredClass("hdl.Bundle")
+    tree.foreachSubTree {
+      case td: TypeDef if td.isClassDef =>
+        val cls = td.symbol.asClass
+        val clsName = cls.name
+        val clsType = cls.typeRef
+
+// println(s"cls: ${cls.show} ${clsName.show} ${clsType}")
+        if cls.thisType.baseClasses.contains(bundleSymbol) && cls != bundleSymbol then
+          bundleOwners += cls
+      case _ =>
+    }
+
+    println(s"bundleOwners ${bundleOwners}")
+
+// tree.stats.foreach(x => println(s"${x}"))
+
+    val newStats = tree.stats.map(x => x)
+    cpy.PackageDef(tree)(tree.pid, newStats)
+
+// tree
   }
 }
