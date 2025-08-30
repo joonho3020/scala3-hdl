2810c075e03fddd3509fb9bf65e1d5450ef4cb0b
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-29 22:36:44 -0700

    Revert "Derive BundleLit"
    This reverts commit 0246958c5cb4540f056eb64764103898e0095ba0.

--- hdl/src/main.scala
+++ hdl/src/main.scala
@@ -6,7 +6,6 @@ import scala.compiletime.ops.int.*
 import scala.annotation.targetName
 import scala.compiletime.erasedValue
 import scala.quoted.*
-import hdl.BundleLit
 
 sealed class Width(val value: Int):
   override def toString: String = s"${value}"
@@ -41,9 +40,13 @@ object Main:
     println(s"${UInt(Width(3))}")
     println(s"${UIntLit(Width(3))(4)}")
 
-    class MyBundle(x: Int, y: Int) extends Bundle derives BundleLit:
+    class MyBundle(x: Int, y: Int) extends Bundle:
       val a = UInt(Width(x))
       val b = UInt(Width(y))
+
+    object MyBundle:
+      transparent inline def lit(inline a: UIntLit, inline b: UIntLit): Any =
+        Bundle.lit[MyBundle]("a" -> a, "b" -> b)
 
     val my_bundle = new MyBundle(2, 3)
 
@@ -51,10 +54,14 @@ object Main:
     println(s"${my_bundle.a}")
     println(s"${my_bundle.b}")
 
-    class NestedBundle(x: Int, y: Int, z: Int) extends Bundle derives BundleLit:
+    class NestedBundle(x: Int, y: Int, z: Int) extends Bundle:
       val width_outer = x + y + z
       val inner = new MyBundle(x, y)
       val outer = UInt(Width(width_outer))
+
+    object NestedBundle:
+      transparent inline def lit(inline inner: Bundle, inline outer: UIntLit): Any =
+        Bundle.lit[NestedBundle]("inner" -> inner, "outer" -> outer)
 
     val nested_bundle = new NestedBundle(2, 3, 4)
     println(s"${nested_bundle.outer}")
@@ -64,18 +71,17 @@ object Main:
     val width1 = 20
     val width2 = 30
 
-    // Temporarily commented out to test macro invocation
-    // val my_bundle_lit = MyBundle.lit(
-    //   a = UIntLit(Width(width1))(3),
-    //   b = UIntLit(Width(width2))(4)
-    // )
-    // println(s"${my_bundle_lit} ${my_bundle_lit.a} ${my_bundle_lit.b}")
+    val my_bundle_lit = MyBundle.lit(
+      a = UIntLit(Width(width1))(3),
+      b = UIntLit(Width(width2))(4)
+    )
+    println(s"${my_bundle_lit} ${my_bundle_lit.a} ${my_bundle_lit.b}")
 
-    // val nested_bundle_lit = NestedBundle.lit(
-    //   inner = MyBundle.lit(a = UIntLit(Width(1))(3), b = UIntLit(Width(2))(4)),
-    //   outer = UIntLit(Width(9))(6)
-    // )
-    // println(s"${nested_bundle_lit} ${nested_bundle_lit.inner.a} ${nested_bundle_lit.outer}")
+    val nested_bundle_lit = NestedBundle.lit(
+      inner = MyBundle.lit(a = UIntLit(Width(1))(3), b = UIntLit(Width(2))(4)),
+      outer = UIntLit(Width(9))(6)
+    )
+    println(s"${nested_bundle_lit} ${nested_bundle_lit.inner.a} ${nested_bundle_lit.outer}")
 
 // Compile error
 // val nested_bundle_lit_2 = NestedBundle.lit(
