f84357b3f0c38d94921403919fbe6bcacef233dc
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-09-01 13:40:28 -0700

    checkpoint

--- hdl3/src/main.scala
+++ hdl3/src/main.scala
@@ -1,7 +1,9 @@
 package hdl3
 
+import compiletime.*
+import scala.deriving.*
+import scala.quoted.*
 import scala.deriving.Mirror
-import scala.compiletime.{constValue, erasedValue, summonInline}
 import scala.language.implicitConversions
 import scala.language.dynamics
 
@@ -45,6 +47,18 @@ object RegImpl:
   given RegImpl[UInt]:
     def asReg(v: UInt): Reg[UInt] = new Reg(v)
 
+  inline def derived[T <: ValueType](using m: Mirror.Of[T]): RegImpl[T] =
+    inline m match
+      case p: Mirror.ProductOf[T] =>
+        val elemRegImpls: List[RegImpl[?]] = summonAll[p.MirroredElemTypes]
+        ???
+      case s: Mirror.SumOf[T] => ???
+
+  private inline def summonAll[T <: Tuple]: List[RegImpl[?]] =
+    inline erasedValue[T] match
+      case _: EmptyTuple => Nil
+      case _: (t *: ts) => summonInline[RegImpl[t]] :: summonAll[ts]
+
 object Reg:
   def apply[V <: ValueType](v: V)(using regimpl: RegImpl[V]) =
     regimpl.asReg(v)
@@ -70,4 +84,3 @@ object Main:
     object MyBundle:
       def apply(w1: Int, w2: Int): MyBundle =
         new MyBundle(a = UInt(Width(w1)), b = UInt(Width(w2)))
-
