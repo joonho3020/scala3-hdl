258dc1126966747ff3634a3abd96866a26a0fe34
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-31 21:49:34 -0700

    WIP

--- hdl2/src/main.scala
+++ hdl2/src/main.scala
@@ -1,5 +1,8 @@
 package hdl2
 
+import scala.deriving.Mirror
+import scala.compiletime.{constValue, erasedValue, summonInline}
+
 object Width:
   opaque type Width = Int
 
@@ -26,18 +29,32 @@ final class UInt(w: Width) extends ValueType:
 final class Clock extends ValueType:
   override def toString(): String = "Clock"
 
-type HostLitValue[V <: ValueType] = V match
-  case UInt => Int | BigInt
-  case Clock => Boolean
-  case _ => Nothing
+trait Bundle extends ValueType
+
 
 sealed trait RefType[V <: ValueType]
 
-class Reg[V <: ValueType](value: V) extends RefType[V]:
-  override def toString(): String = s"Reg($value)"
+class Reg[V <: ValueType](v: V) extends RefType[V]:
+  override def toString(): String = s"Reg(${v})"
 
-class Lit[V <: ValueType](value: V)(lit: HostLitValue[V]) extends RefType[V]:
-  override def toString(): String = s"$value($lit)"
+class Lit[V <: ValueType](v: V)(using h: HostLit[V])(lit: h.Repr) extends RefType[V]:
+  override def toString(): String = s"Lit(${v})"
+
+object Lit:
+  def apply[V <: ValueType, R](v: V, r: R)(using HostLit.Aux[V, R]): Lit[V] =
+    new Lit[V](v)(using summon)(r)
+
+trait HostLit[V <: ValueType]:
+  type Repr
+
+object HostLit:
+  type Aux[V <: ValueType, R] = HostLit[V] { type Repr = R }
+
+  given uint: Aux[UInt, Int | BigInt] = new HostLit[UInt]{ type Repr = Int | BigInt }
+  given clock: Aux[Clock, Boolean]    = new HostLit[Clock]{ type Repr = Boolean }
+
+  // given uint: HostLit[UInt] = new HostLit[UInt]{ type Repr = Int | BigInt }
+  // given clock: HostLit[Clock] = new HostLit[Clock]{ type Repr = Boolean }
 
 object Main:
   def main(args: Array[String]): Unit =
@@ -46,11 +63,11 @@ object Main:
     val a = UInt(Width(2))
     println(a)
 
-    val r1 = Reg(a)
+    val r1 = Reg(UInt(Width(4)))
     println(r1)
 
-    val r2 = Reg(Clock())
-    println(r2)
+    val uhost = summon[HostLit[UInt]]
+    println(uhost)
 
-    val l1 = Lit(UInt(Width(3)))(4)
+    val l1 = Lit(UInt(Width(4)), 2 : Int | BigInt)
     println(l1)
