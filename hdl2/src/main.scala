80fad60c44647011c6cf010681a5acc4d186f663
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-08-31 20:47:25 -0700

    Constraint host literal type

--- hdl2/src/main.scala
+++ hdl2/src/main.scala
@@ -26,15 +26,17 @@ final class UInt(w: Width) extends ValueType:
 final class Clock extends ValueType:
   override def toString(): String = "Clock"
 
-
-
+type HostLitValue[V <: ValueType] = V match
+  case UInt => Int | BigInt
+  case Clock => Boolean
+  case _ => Nothing
 
 sealed trait RefType[V <: ValueType]
 
 class Reg[V <: ValueType](value: V) extends RefType[V]:
   override def toString(): String = s"Reg($value)"
 
-class Lit[V <: ValueType](value: V)(lit: Int) extends RefType[V]:
+class Lit[V <: ValueType](value: V)(lit: HostLitValue[V]) extends RefType[V]:
   override def toString(): String = s"$value($lit)"
 
 object Main:
