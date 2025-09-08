5027bc20d792523061c7711f939c2421ef4e6b88
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-09-07 20:43:58 -0700

    Not working so well

--- hdl8/src/lit.scala
+++ hdl8/src/lit.scala
@@ -12,21 +12,26 @@ type HostTypeOf[T] = T match
   case _    => NamedTuple.Map[NTOf[T], [X] =>> HostTypeOf[X & ValueType]]
 
 final class Lit[T](private val payload: Any) extends Selectable:
-  type Fields = NamedTuple.Map[
-    NTOf[T],
-    [X] =>> Lit[X & ValueType]
+  type Fields = NamedTuple.NamedTuple[
+    ShapeOf[T]#Labels,
+    MapElems[ShapeOf[T]#Elems, [X] =>> Lit[X]]
   ]
 
   inline def selectDynamic(name: String): Lit[?] =
-    summonFrom {
-      case m: Mirror.ProductOf[T] =>
-        val labels = constValueTuple[m.MirroredElemLabels].toArray
-        val idx = labels.indexOf(name)
-        val subpayload = payload.asInstanceOf[Product].productElement(idx)
-        new Lit[Any](subpayload)
-      case _ =>
-        throw new NoSuchElementException(s"${summonInline[ValueOf[String]]}") // never reached, just keep total
-    }
+    val s = summonInline[ShapeOf[T]]
+    val idx = s.labels.indexOf(name)
+    val p = payload.asInstanceOf[Product]
+    new Lit[Any](p.productElement(idx))
+
+// summonFrom {
+// case m: Mirror.ProductOf[T] =>
+// val labels = constValueTuple[m.MirroredElemLabels].toArray
+// val idx = labels.indexOf(name)
+// val subpayload = payload.asInstanceOf[Product].productElement(idx)
+// new Lit[Any](subpayload)
+// case _ =>
+// throw new NoSuchElementException(s"${summonInline[ValueOf[String]]}") // never reached, just keep total
+// }
   transparent inline def get: HostTypeOf[T] =
     payload.asInstanceOf[HostTypeOf[T]]
 
--- hdl8/src/main.scala
+++ hdl8/src/main.scala
@@ -1,234 +1,5 @@
 package hdl8
 
-// import scala.deriving.Mirror
-// import scala.compiletime.{constValueTuple, summonInline, summonFrom}
-// import scala.util.Try
-// import scala.language.dynamics
-// import scala.NamedTuple
-// import scala.NamedTuple.Map
-// import scala.NamedTuple.From
-
-// trait Reg[T] extends Selectable:
-//   type Fields = NamedTuple.Map[NamedTuple.From[T], Reg]
-//   def selectDynamic(fieldName: String): Reg[?]
-// 
-// final case class RegLeaf[A](payload: A) extends Reg[A]:
-//   def selectDynamic(fieldName: String): Reg[?] =
-//     throw NoSuchFieldException(s"'$fieldName' on leaf (${payload.getClass.getName})")
-// 
-// final case class RegNode[T](value: T)(using m: Mirror.ProductOf[T]) extends Reg[T]:
-//   private def labels: List[String] =
-//     value.asInstanceOf[Product].productElementNames.toList
-// 
-//   def selectDynamic(fieldName: String): Reg[?] =
-//     val i = labels.indexOf(fieldName)
-//     if i < 0 then
-//       throw NoSuchFieldException(s"'$fieldName' not in ${labels.mkString("[", ", ", "]")}")
-//     val elem = value.asInstanceOf[Product].productElement(i)
-//     Reg(elem)
-// 
-// object Reg:
-//   inline def apply[T](value: T): Reg[T] =
-//     summonFrom {
-//       case m: Mirror.ProductOf[T] => RegNode[T](value)(using m)
-//       case _                      => RegLeaf[T](value)
-//     }
-// 
-// object Main:
-//   def main(args: Array[String]): Unit =
-//     println("Hello World")
-// 
-//     case class MyBundle(a: Int, b: Int)
-//     val mybundle = MyBundle(2, 3)
-// 
-// // val reg_mybundle: Reg[MyBundle]
-// // reg_mybundle.a
-//     val reg_mybundle = Reg(mybundle)
-//     val reg_a = reg_mybundle.a
-//     println(s"reg_mybundle.a => ${reg_mybundle.a}")
-
-
-// import compiletime.*
-// import scala.deriving.*
-// import scala.NamedTuple
-// 
-// trait Bundle
-// 
-// case class MyBundle(a: Int, b: Boolean) extends Bundle
-// 
-// trait Reg[T] extends Selectable:
-//   type Fields
-//   def regs: Map[String, Reg[?]]
-//   inline def selectDynamic(name: String): Reg[?]
-// 
-// given Reg[Int]:
-//   type Fields = ()
-//   def regs: Map[String, Reg[?]] = Map()
-//   inline def selectDynamic(name: String): Reg[?] =
-//     sys.error(s"Leaf bundle does not have subfield access to ${name}")
-// // 
-// // given Reg[MyBundle](x: MyBundle):
-// //   type Fields = (
-// //     a: Reg[Int],
-// //     b: Reg[Boolean]
-// //   )
-// //   def regs = Map(
-// //     "a" -> Reg[Int](x.a),
-// //     "b" -> Reg[Boolean](x.b)
-// //   )
-// //   inline def selectDynamic(name: String): Reg[?] =
-// //     regs.getOrElse(
-// //       name,
-// //       sys.error(s"Invalid field name ${name}"))
-// //   def getValue: MyBundle = x
-// 
-// ///////////////////////
-// 
-// @main
-// def example(): Unit =
-//   import hdl8.*
-// 
-// 
-// // object MyBundleReg:
-// // val a = Reg[Int](_.a)
-// // val b = Reg[Boolean](_.b)
-// 
-//   val mybundle = MyBundle(42, true)
-
-// import scala.deriving.*
-// import scala.compiletime.*
-// import scala.NamedTuple
-// import scala.util.NotGiven
-// 
-// trait Bundle
-// 
-// trait Ref[T] extends Selectable:
-//   type Fields
-//   def selectDynamic(name: String): Ref[?] =
-//     sys.error(s"Ref[T] has no field '$name'") // overridden for products
-//   def get: T
-//   def set(t: T): Unit
-// 
-// final class Register[T](private var v: T) extends Ref[T]:
-//   type Fields = EmptyTuple
-//   override def get: T = v
-//   override def set(t: T): Unit = v = t
-// 
-// trait MakeLeaf[T]:
-//   def apply(init: T): Ref[T]
-// 
-// object MakeLeaf:
-//   given [T]: MakeLeaf[T] with
-//     def apply(init: T): Ref[T] = Register(init)
-// 
-// final class ProductRef[T](
-//   val fieldsMap: Map[String, Ref[?]],
-//   val toValue: () => T,
-//   val fromValue: T => Unit
-// ) extends Ref[T]:
-//   type Fields = NamedTuple.Map[NamedTuple.From[T], Ref]
-//   override def selectDynamic(name: String): Ref[?] =
-//     fieldsMap.getOrElse(name, sys.error(s"Invalid field name: $name"))
-//   transparent inline def at[K <: String](using k: ValueOf[K]): Ref[Types.FieldTypeByName[T, K]] =
-//     fieldsMap.getOrElse(k.value, sys.error(s"Invalid field name: ${k.value}")).asInstanceOf[Ref[Types.FieldTypeByName[T, K]]]
-//   def get: T = toValue()
-//   def set(t: T): Unit = fromValue(t)
-// 
-// object Types:
-//   type ElemTypeByLabel[Labels <: Tuple, Types <: Tuple, K <: String] = (Labels, Types) match
-//     case (K *: _, h *: _) => h
-//     case (_ *: lt, _ *: tt) => ElemTypeByLabel[lt, tt, K]
-//     case (EmptyTuple, EmptyTuple) => Nothing
-// 
-//   type FieldTypeByName[T, K <: String] = ElemTypeByLabel[
-//     Mirror.ProductOf[T]#MirroredElemLabels,
-//     Mirror.ProductOf[T]#MirroredElemTypes,
-//     K
-//   ]
-// 
-// trait MakeRef[T]:
-//   type Out <: Ref[T]
-//   def apply(init: T): Out
-// 
-// object MakeRef:
-//   // Leaves: Int, Boolean, etc.
-//   inline given leaf[T](using ev: NotGiven[Mirror.ProductOf[T]], ml: MakeLeaf[T]): MakeRef[T] =
-//     new MakeRef[T]:
-//       type Out = Ref[T]
-//       def apply(init: T): Out =
-//         println(s"MakeRef leaf ${init}")
-//         ml.apply(init)
-// 
-//   // Products (case classes)
-//   inline given product[T <: Bundle](using p: Mirror.ProductOf[T]): MakeRef[T] =
-//     println(s"MakeRef product")
-//     val labelsCT: List[String] = constValueTuple[p.MirroredElemLabels].toList.asInstanceOf[List[String]]
-//     println(s"labelsCT ${labelsCT}")
-//     val elemMakersCT: List[MakeRef[Any]] = summonAllElems[p.MirroredElemTypes]
-//     new MakeRef[T]:
-//       type Out = ProductRef[T]
-//       def apply(init: T): Out =
-//         val childRefs: List[Ref[?]] =
-//           elemMakersCT.zipWithIndex.map { case (mk, i) =>
-//             val prod = init.asInstanceOf[Product]
-//             val childInit = prod.productElement(i)
-//             mk(childInit)
-//           }
-// 
-//         val fields = labelsCT.zip(childRefs).toMap
-// 
-//         def toT(): T =
-//           val elems = childRefs.map(_.get).asInstanceOf[List[Any]]
-//           p.fromProduct(Tuple.fromArray(elems.toArray))
-// 
-//         def fromT(t: T): Unit =
-//           val prod = t.asInstanceOf[Product]
-//           var i = 0
-//           while i < childRefs.length do
-//             childRefs(i).asInstanceOf[Ref[Any]].set(prod.productElement(i))
-//             i += 1
-// 
-//         println(s"fields ${fields}")
-//         ProductRef[T](fields, () => toT(), fromT)
-// 
-//   // Helper: summon MakeRef for each element in a tuple type
-//   private inline def summonAllElems[T <: Tuple]: List[MakeRef[Any]] =
-//     inline erasedValue[T] match
-//       case _: EmptyTuple => Nil
-//       case _: (h *: t)   => summonInline[MakeRef[h]].asInstanceOf[MakeRef[Any]] :: summonAllElems[t]
-// 
-// // object MakeLeafRef:
-// // Leaves: Int, Boolean, etc.
-// // inline given leaf[T](using ev: NotGiven[Mirror.ProductOf[T]], ml: MakeLeaf[T]): MakeProductRef[T] with
-// // def apply(init: T): Ref[T] =
-// // println(s"MakeProductRef leaf ${init}")
-// // ml.apply(init)
-// 
-// object HDL:
-//   inline def reg[T](init: T)(using mk: MakeRef[T]): mk.Out =
-//     mk(init).asInstanceOf[mk.Out]
-// 
-// case class Inner(x: Int, y: Boolean) extends Bundle
-// case class MyBundle(a: Int, b: Boolean, inner: Inner) extends Bundle
-// 
-// @main def demo(): Unit =
-//   import HDL.*
-// 
-//   val t = reg[Inner](Inner(2, true))
-//   t.x
-// 
-//   val r = reg[MyBundle](MyBundle(3, true, Inner(2, false)))
-//   r.a
-// // r.a.set(42)
-// // r.b.set(true)
-// // r.inner.x.set(7)
-// // r.inner.y.set(false)
-// 
-//   val v1 = r.get
-//   println(s"v1 ${v1}")
-//   r.set(MyBundle(1, false, Inner(99, true)))
-
-
 import scala.deriving.*
 import scala.compiletime.*
 import scala.NamedTuple
@@ -251,26 +22,36 @@ sealed class Bool extends ValueType
 
 trait Bundle extends ValueType
 
+type MapElems[E <: Tuple, F[_]] <: Tuple = E match
+  case EmptyTuple => EmptyTuple
+  case h *: t     => F[h & ValueType] *: MapElems[t, F]
+
 type NTOf[T] <: NamedTuple.AnyNamedTuple = T match
   case UInt | Bool => NamedTuple.NamedTuple[EmptyTuple, EmptyTuple]
-  case _           => NamedTuple.From[T]
+  case _           => NamedTuple.NamedTuple[ShapeOf[T]#Labels, ShapeOf[T]#Elems]
 
-final class Reg[T](val t: T) extends Selectable:
-  type Fields = NamedTuple.Map[
-    NamedTuple.From[T],
-    [X] =>> Reg[X & ValueType]]
+final class Reg[T](val t: T)(using s: ShapeOf[T]) extends Selectable:
+  val shape = s
+
+  type Fields = NamedTuple.NamedTuple[
+    shape.Labels,
+    MapElems[shape.Elems, [X] =>> Reg[X]]
+  ]
 
   inline def selectDynamic(name: String): Reg[?] =
-    summonFrom {
-      case m: Mirror.ProductOf[T] =>
-        val labels = constValueTuple[m.MirroredElemLabels].toArray
-        val idx = labels.indexOf(name)
-        println(s"labels: ${labels} name: ${name} idx: ${idx}")
-        val child = t.asInstanceOf[Product].productElement(idx).asInstanceOf[ValueType]
-        new Reg(child)
-      case _ =>
-        throw new NoSuchElementException(s"${t.getClass.getName} has no field '$name'")
-    }
+    val child = shape.getVT(t, name)
+    new Reg(child)
+
+// summonFrom {
+// case m: Mirror.ProductOf[T] =>
+// val labels = constValueTuple[m.MirroredElemLabels].toArray
+// val idx = labels.indexOf(name)
+// val child = t.asInstanceOf[Product].productElement(idx).asInstanceOf[ValueType]
+// new Reg(child)
+// case _ =>
+// throw new NoSuchElementException(s"${t.getClass.getName} has no field '$name'")
+// }
+
   override def toString(): String =
     s"Reg(${t})"
 
@@ -308,60 +89,64 @@ final class Reg[T](val t: T) extends Selectable:
   final case class MyBundle(x: UInt, y: UInt, i: InnerBundle) extends Bundle
   val mb = MyBundle(UInt(Width(2)), UInt(Width(3)), InnerBundle(UInt(Width(4)), UInt(Width(5))))
 
+// val s = summonInline[ShapeOf[MyBundle]]
+// println(s"s.labels ${s.labels}")
+
   val reg = Reg(mb)
+  println(showTypeOf[Reg[MyBundle]#Fields])
   val reg_x: Reg[UInt] = reg.x
-  val reg_y: Reg[UInt] = reg.y
-  // val reg_y: Reg[UIntLit] = reg.y // Type mismatch, doesn't compile
-  val reg_i: Reg[InnerBundle] = reg.i
-  // val reg_i: Reg[UIntLit] = reg.i // Type mismatch doesn't compile
-  val reg_i_a: Reg[UInt] = reg_i.a
-  // val reg_i_a: Reg[UIntLit] = reg_i.a // Type mismatch doesn't compile
+  // val reg_y: Reg[UInt] = reg.y
+  // // val reg_y: Reg[UIntLit] = reg.y // Type mismatch, doesn't compile
+  // val reg_i: Reg[InnerBundle] = reg.i
+  // // val reg_i: Reg[UIntLit] = reg.i // Type mismatch doesn't compile
+  // val reg_i_a: Reg[UInt] = reg_i.a
+  // // val reg_i_a: Reg[UIntLit] = reg_i.a // Type mismatch doesn't compile
 
-  val reg_i_b: Reg[UInt] = reg.i.b
-  // val reg_i_b: Reg[UIntLit] = reg.i.b // Type mismatch doesn't compile
-  println(s"reg_x: ${reg_x} reg_y: ${reg_y} reg_i: ${reg_i} reg_i_a ${reg_i_a} reg_i_b ${reg_i_b}")
+  // val reg_i_b: Reg[UInt] = reg.i.b
+  // // val reg_i_b: Reg[UIntLit] = reg.i.b // Type mismatch doesn't compile
+  // println(s"reg_x: ${reg_x} reg_y: ${reg_y} reg_i: ${reg_i} reg_i_a ${reg_i_a} reg_i_b ${reg_i_b}")
 
-  val ulit = Lit[UInt](3)
-  println(s"ulit.get: ${ulit.get}")
+  // val ulit = Lit[UInt](3)
+  // println(s"ulit.get: ${ulit.get}")
 
-  val inner_bundle_host_type: HostTypeOf[InnerBundle] = (
-    a = 3,
-    b = 2,
-  )
   // val inner_bundle_host_type: HostTypeOf[InnerBundle] = (
   //   a = 3,
   //   b = 2,
-  //   c = 4
-  // ) // compile fails, type mismatch
+  // )
+  // // val inner_bundle_host_type: HostTypeOf[InnerBundle] = (
+  // //   a = 3,
+  // //   b = 2,
+  // //   c = 4
+  // // ) // compile fails, type mismatch
 
-  println(s"inner_bundle_host_type ${inner_bundle_host_type}")
+  // println(s"inner_bundle_host_type ${inner_bundle_host_type}")
 
 
-  val ilit = Lit[InnerBundle]((a = 3, b = 4))
+  // val ilit = Lit[InnerBundle]((a = 3, b = 4))
 
-  val ilit_a: Lit[UInt] = ilit.a
+  // val ilit_a: Lit[UInt] = ilit.a
 
-  // val ilit_a: Lit[Bool] = ilit.a // Type mismatch doesn't compile
+  // // val ilit_a: Lit[Bool] = ilit.a // Type mismatch doesn't compile
 
-  println(s"ilit.a ${ilit.a.get} ilit_a.get ${ilit_a.get}")
+  // println(s"ilit.a ${ilit.a.get} ilit_a.get ${ilit_a.get}")
 
-  val mylit = Lit[MyBundle]((
-    x = 2,
-    y = 3,
-    i = (a = 4, b = 5)))
-
-  val mylit_x: Lit[UInt] = mylit.x
-  println(s"mylit_x.get ${mylit_x.get} mylit.x.get ${mylit.x.get}")
-
-  val mylit_i: Lit[InnerBundle] = mylit.i
-// val mylit_i: Lit[MyBundle] = mylit.i // Type mismatch doesn't compile
-// val mylit_i: Lit[UInt] = mylit.i // Type mismatch doesn't compile
-  println(s"mylit_i.get ${mylit_i.get} mylit.i.get ${mylit.i.get}")
-
-  val mylit_i_a: Lit[UInt] = mylit.i.a
-  println(s"mylit_i_a.get ${mylit_i_a.get} ${mylit.i.a.get} ${mylit_i.a.get}")
-
-  // val mylit_2 = Lit[MyBundle]((
-  //   y = 3,
+  // val mylit = Lit[MyBundle]((
   //   x = 2,
-  //   i = (a = 4, b = 5))) // Doesn't compile because we mixed up the order of named tuples
+  //   y = 3,
+  //   i = (a = 4, b = 5)))
+
+  // val mylit_x: Lit[UInt] = mylit.x
+  // println(s"mylit_x.get ${mylit_x.get} mylit.x.get ${mylit.x.get}")
+
+  // val mylit_i: Lit[InnerBundle] = mylit.i
+////  val mylit_i: Lit[MyBundle] = mylit.i // Type mismatch doesn't compile
+////  val mylit_i: Lit[UInt] = mylit.i // Type mismatch doesn't compile
+  // println(s"mylit_i.get ${mylit_i.get} mylit.i.get ${mylit.i.get}")
+
+  // val mylit_i_a: Lit[UInt] = mylit.i.a
+  // println(s"mylit_i_a.get ${mylit_i_a.get} ${mylit.i.a.get} ${mylit_i.a.get}")
+
+  // // val mylit_2 = Lit[MyBundle]((
+  // //   y = 3,
+  // //   x = 2,
+  // //   i = (a = 4, b = 5))) // Doesn't compile because we mixed up the order of named tuples
