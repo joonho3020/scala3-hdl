6bb7fd0658ae41a1618a655dc61b8d05eb1105f1
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-09-07 21:01:53 -0700

    Revert "Not working so well"
    This reverts commit 5027bc20d792523061c7711f939c2421ef4e6b88.

--- hdl8/src/lit.scala
+++ hdl8/src/lit.scala
@@ -12,26 +12,21 @@ type HostTypeOf[T] = T match
   case _    => NamedTuple.Map[NTOf[T], [X] =>> HostTypeOf[X & ValueType]]
 
 final class Lit[T](private val payload: Any) extends Selectable:
-  type Fields = NamedTuple.NamedTuple[
-    ShapeOf[T]#Labels,
-    MapElems[ShapeOf[T]#Elems, [X] =>> Lit[X]]
+  type Fields = NamedTuple.Map[
+    NTOf[T],
+    [X] =>> Lit[X & ValueType]
   ]
 
   inline def selectDynamic(name: String): Lit[?] =
-    val s = summonInline[ShapeOf[T]]
-    val idx = s.labels.indexOf(name)
-    val p = payload.asInstanceOf[Product]
-    new Lit[Any](p.productElement(idx))
-
-// summonFrom {
-// case m: Mirror.ProductOf[T] =>
-// val labels = constValueTuple[m.MirroredElemLabels].toArray
-// val idx = labels.indexOf(name)
-// val subpayload = payload.asInstanceOf[Product].productElement(idx)
-// new Lit[Any](subpayload)
-// case _ =>
-// throw new NoSuchElementException(s"${summonInline[ValueOf[String]]}") // never reached, just keep total
-// }
+    summonFrom {
+      case m: Mirror.ProductOf[T] =>
+        val labels = constValueTuple[m.MirroredElemLabels].toArray
+        val idx = labels.indexOf(name)
+        val subpayload = payload.asInstanceOf[Product].productElement(idx)
+        new Lit[Any](subpayload)
+      case _ =>
+        throw new NoSuchElementException(s"${summonInline[ValueOf[String]]}") // never reached, just keep total
+    }
   transparent inline def get: HostTypeOf[T] =
     payload.asInstanceOf[HostTypeOf[T]]
 
