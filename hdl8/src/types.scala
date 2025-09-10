3e6fe0dc602b072cf3ef0463129dedb8a7255223
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-09-09 17:22:48 -0700

    Cleanup before adding directionality

--- hdl8/src/types.scala
+++ hdl8/src/types.scala
@@ -6,6 +6,36 @@ import scala.NamedTuple
 import scala.util.NotGiven
 import scala.quoted.*
 
+final class Reg[T](val t: T) extends Selectable:
+  type Fields = NamedTuple.Map[
+    NamedTuple.From[T],
+    [X] =>> Reg[X & ValueType]]
+
+  inline def selectDynamic(name: String): Reg[?] =
+    summonFrom {
+      case m: Mirror.ProductOf[T] =>
+        val labels = constValueTuple[m.MirroredElemLabels].toArray
+        val idx = labels.indexOf(name)
+        val child = t.asInstanceOf[Product].productElement(idx).asInstanceOf[ValueType]
+        new Reg(child)
+      case _ =>
+        throw new NoSuchElementException(s"${t.getClass.getName} has no field '$name'")
+    }
+  override def toString(): String =
+    s"Reg(${t})"
+
+object Reg:
+  def apply[T <: ValueType](t: T): Reg[T] = new Reg(t)
+
+object RegVecOps:
+  extension [A <: ValueType](rv: Reg[Vec[A]])
+    def apply(index: Int): Reg[A] =
+      new Reg(rv.t.elem)
+
+    def apply(start: Int, end: Int): Reg[Vec[A]] =
+      val sliceLen = (end - start) + 1
+      new Reg(Vec(rv.t.elem, sliceLen))
+
 type HostTypeOf[T] = T match
   case UInt    => Int
   case Bool    => Boolean
---
+++ hdl8/src/value.scala
@@ -0,0 +1,37 @@
+
+package hdl8
+
+import scala.deriving.*
+import scala.compiletime.*
+import scala.NamedTuple
+import scala.util.NotGiven
+import scala.quoted.*
+
+sealed class Width(val value: Int):
+  override def toString: String = s"${value}"
+
+object Width:
+  def apply(x: Int): Width = new Width(x)
+
+sealed trait ValueType
+
+sealed class UInt(val w: Width) extends ValueType:
+  override def toString(): String = s"UInt($w.W)"
+
+object UInt:
+  def apply(w: Width): UInt = new UInt(w)
+
+sealed class Bool extends ValueType:
+  override def toString(): String = s"Bool()"
+
+object Bool:
+  def apply(): Bool = new Bool
+
+trait Bundle extends ValueType
+
+final class Vec[T <: ValueType](val elem: T, val len: Int) extends ValueType:
+  override def toString(): String = s"Vec($elem, $len)"
+
+object Vec:
+  def apply[T <: ValueType](elem: T, len: Int): Vec[T] = new Vec(elem, len)
+  // TODO: Add foreach, map...
