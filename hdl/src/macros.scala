0bd4d3ff4a5c3faba7962a449e98d220665c8259
joonho.whangbo <joonho.whangbo@berkeley.edu>
2025-12-01 11:45:33 -0800

    wip

--- hdl/src/macros.scala
+++ hdl/src/macros.scala
@@ -44,8 +44,11 @@ object InstanceMacros:
   /** Creates an Instance with typed IO accessors.
    *  The macro inspects the module type M to find all IO[T] fields
    *  and generates InstancePort[T] accessors with proper types.
+   *
+   *  Uses `transparent inline` so the compiler infers the refined return type
+   *  that includes the `io` field with its proper type.
    */
-  inline def createInstance[M <: Module](child: M)(using ctx: ElabContext): Instance[M] =
+  transparent inline def createInstance[M <: Module](child: M)(using ctx: ElabContext): Instance[M] =
     ${ createInstanceImpl[M]('child, 'ctx) }
 
   private def createInstanceImpl[M <: Module: Type](
@@ -55,53 +58,48 @@ object InstanceMacros:
     import quotes.reflect.*
 
     val moduleType = TypeRepr.of[M]
-    val ioType = TypeRepr.of[IO]
 
-    // Find all val fields in M that have type IO[T] for some T
-    val ioFields = moduleType.typeSymbol.fieldMembers.filter { field =>
-      field.tree match
-        case ValDef(_, tpt, _) =>
-          val fieldType = tpt.tpe
-          fieldType <:< ioType.appliedTo(TypeRepr.of[ValueType])
-        case _ => false
-    }
+    report.info(s"Module type: ${moduleType.show}")
 
-    // For each IO field, extract its inner type T
-    case class IOFieldInfo(name: String, innerType: TypeRepr)
-
-    val fieldInfos = ioFields.map { field =>
-      val fieldType = moduleType.memberType(field)
-      // Extract T from IO[T]
-      val innerType = fieldType match
-        case AppliedType(_, List(t)) => t
-        case _ => report.errorAndAbort(s"Expected IO[T] type for field ${field.name}, got $fieldType")
-      IOFieldInfo(field.name, innerType)
-    }
-
-    // Generate the Instance creation with typed IO accessors
-    // We create an anonymous class that extends Instance[M] with the IO accessors
-    if fieldInfos.isEmpty then
-      // No IO fields, just create a plain Instance
-      '{ $ctx.instantiate($child) }
-    else
-      // Create Instance with typed IO accessor(s)
-      // For now, we handle the common case of a single 'io' field
-      fieldInfos.find(_.name == "io") match
-        case Some(IOFieldInfo(_, innerType)) =>
+    def generateWithIO(ioFieldType: TypeRepr): Expr[Instance[M]] =
+      ioFieldType match
+        case AppliedType(_, List(innerType)) =>
+          report.info(s"Inner type: ${innerType.show}")
           innerType.asType match
             case '[t] =>
+              // Generate code that accesses the io field directly using Select
+              val ioAccessExpr = Select.unique(child.asTerm, "io").asExprOf[IO[t & ValueType]]
               '{
-                val inst = $ctx.instantiate($child)
-                new Instance[M](inst.name, inst.module, inst.moduleIR) {
-                  val io: InstancePort[t & ValueType] =
-                    new InstancePort[t & ValueType](
+                val childModule = $child
+                val childIO = $ioAccessExpr
+                val inst = $ctx.instantiate(childModule)
+                new hdl.Instance[M](inst.name, inst.module, inst.moduleIR) {
+                  val io: hdl.InstancePort[t & ValueType] =
+                    new hdl.InstancePort[t & ValueType](
                       inst.name,
-                      inst.module.asInstanceOf[{ val io: IO[t & ValueType] }].io.t,
-                      inst.module.asInstanceOf[{ val io: IO[t & ValueType] }].io.name
+                      childIO.t,
+                      childIO.name
                     )
                 }
               }
-        case None =>
-          // No 'io' field, create plain Instance
-          // User can access other IO fields via the apply method
+        case other =>
+          report.info(s"io field has unexpected type structure: ${other.show}")
           '{ $ctx.instantiate($child) }
+
+    val ioSymbol = moduleType.typeSymbol.fieldMember("io")
+
+    if ioSymbol.isNoSymbol then
+      report.info(s"No 'io' field found via fieldMember, trying declaredField")
+      // Try declaredField for local classes
+      val declaredIo = moduleType.typeSymbol.declaredField("io")
+      if declaredIo.isNoSymbol then
+        report.info(s"No 'io' field found, using fallback")
+        '{ $ctx.instantiate($child) }
+      else
+        val ioFieldType = moduleType.memberType(declaredIo)
+        report.info(s"Found io via declaredField, type: ${ioFieldType.show}")
+        generateWithIO(ioFieldType)
+    else
+      val ioFieldType = moduleType.memberType(ioSymbol)
+      report.info(s"Found io via fieldMember, type: ${ioFieldType.show}")
+      generateWithIO(ioFieldType)
